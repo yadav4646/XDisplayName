33eb20ec2a610efbb352ac96bb08e6832242c8bb
Kuldeep Yadav <64552714+yadav4646@users.noreply.github.com>
2024-12-06 22:57:21 +0530

    First

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+function App() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [fullName, setFullName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (firstName && lastName) {
+      setFullName(`${firstName} ${lastName}`);
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>Full Name Display</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="firstName">First Name </label>
+          <input
+            type="text"
+            required
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="lastName">Last Name </label>
+          <input
+            type="text"
+            required
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+        </div>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+      {fullName && <h3>Full name: {fullName}</h3>}
+    </div>
+  );
+}
+
+export default App;
